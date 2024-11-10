40410958984d266fdd892d43dc71c8ce08f15341
samuel colman <samucol2004@gmail.com>
2024-11-10 11:20:07 -0500

    fourth commit

--- proyecto/base/urls.py
+++ proyecto/base/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import ListaPendientes, DetalleTarea, CrearTarea
+from .views import ListaPendientes, DetalleTarea, CrearTarea, EditarTarea, EliminarTarea, Logueo
+from django.contrib.auth import views as auth_views
 
-urlpatterns = [path('', ListaPendientes.as_view(), name='pendientes'),
+urlpatterns = [path('', ListaPendientes.as_view(), name='tareas'),
+               path('login/', Logueo.as_view(), name='login'),
+               path('logout/', auth_views.LogoutView.as_view(), name='logout'),
                path('tarea/<int:pk>', DetalleTarea.as_view(), name='tarea'),
-               path('crear-tarea/', CrearTarea.as_view(), name='crear-tarea'),]
+               path('crear-tarea/', CrearTarea.as_view(), name='crear-tarea'),
+               path('editar-tarea/<int:pk>', EditarTarea.as_view(), name='editar-tarea'),
+               path('eliminar-tarea/<int:pk>', EliminarTarea.as_view(), name="eliminar-tarea")]
